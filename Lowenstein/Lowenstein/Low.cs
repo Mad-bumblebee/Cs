c69f5f926fdedfd10f0f387a8196f652bc94caad
Mad-bumblebee <45847090+Mad-bumblebee@users.noreply.github.com>
2019-04-10 15:30:49 +0300

    Update

--- Lowenstein/Lowenstein/Low.cs
+++ Lowenstein/Lowenstein/Low.cs
@@ -29,18 +29,19 @@ namespace Lowenstein
             for (int i = 0; i <= N ; i++) matrix[0, i] = i;           
            
             for (int i = 1; i <= M; i++) matrix[i, 0] = i;
-           
+
+            bool price;
 
             for (int i = 1; i <= N; i++)
             {
                 s = str1[i-1];
                 
-                for (int j = 1,price; j <= M;j++)
+                for (int j = 1; j <= M;j++)
                 {
                     t = str2[j-1];
-                    price = (t == s) ? 0 : 1;
+                    price = (t == s) ? false : true;
                     
-                    matrix[j, i] = Math.Min(Math.Min(matrix[j,i-1] + 1,matrix[j-1,i] +1) ,matrix[j-1,i-1] + Convert.ToInt32(price));
+                    matrix[j, i] = Math.Min(Math.Min(matrix[j,i-1] + 1,matrix[j-1,i] +1) ,matrix[j-1,i-1] + Convert.ToByte(price));
                 }
             }          
 
